5ea838a343374266f3d71ade2974419b68f56aa9
Juan <juclozanome@unal.edu.co>
2018-10-21 23:33:58 -0500

    image microservice reboot

--- server.js
+++ server.js
@@ -2,16 +2,24 @@ var redis = require('redis');
 var express = require("express");
 var fileUpload = require("express-fileupload");
 var bodyParser = require('body-parser');
-var client = redis.createClient(6379,'image-db');
+var cloudinary = require('cloudinary');
+var client = redis.createClient();
 var app = express();
 var path = require('path');
 var fs = require('fs');
 //app.use(bodyParser.json());
+
+cloudinary.config({
+  cloud_name: 'olimita',
+  api_key: '471581619822586',
+  api_secret: 'qm1kwv1rcBk4jIdTXQymLASXAL4'
+});
+
 app.use(fileUpload());
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var server = app.listen(3000, function () {
+var server = app.listen(3003, function () {
     console.log('Server is running..');
 });
 
@@ -21,9 +29,7 @@ client.on('error', function(err){
 
 app.get('/image/:id/', function(req, res) {
   client.get(req.params.id,function(error, result){
-    if (fs.existsSync(result)) {
-			res.sendFile(path.join(result));
-		}else{return res.status(404).send('Image not found.');}
+    res.send(result);
   });
 });
 
@@ -31,7 +37,7 @@ app.post('/image/', function(req, res) {
   if (!req.files)
     return res.status(400).send('No files were uploaded.');
   let sampleFile = req.files.file;
-	sampleFile.name = req.body.project +'v'+ req.body.version +'p'+ req.body.page + '.png';
+  sampleFile.name = req.body.project +'v'+ req.body.version +'p'+ req.body.page + '.png';
 	uploadPath = __dirname + '/uploads/' + sampleFile.name;
   client.incr("counter");
   sampleFile.mv(uploadPath, function(err) {
@@ -40,36 +46,47 @@ app.post('/image/', function(req, res) {
     }
     client.get("counter", function(error, result) {
       if (error) throw error;
-      client.set(result, uploadPath, redis.print)
-      res.send('El archivo fue guardado con el codigo ' + result);
+      var numero = result
+      cloudinary.v2.uploader.upload(uploadPath,{public_id: numero},
+        function(err, result) {
+          console.log(result)
+          client.set(numero, result.url, redis.print)
+        });
+    });
+    client.get("counter", function(error, result) {
+      if (error) throw error;
+      res.send(result);
     });
   });
 });
 
 app.put('/image/:id/', function(req, res) {
-	if (!req.files)
-    return res.status(400).send('Missing image to update.');
+  if (!req.files)
+    return res.status(400).send('No image to upload.');
   let sampleFile = req.files.file;
-  client.get(req.params.id,function(error, result){
-    if (fs.existsSync(result)) {
-      sampleFile.mv(result, function(err) {
-      if (err) {
-        return res.status(500).send(err);
-      }
-      res.send('El archivo con codigo ' + req.params.id + ' ha sido actualizado');
+	uploadPath = __dirname + '/uploads/' + sampleFile.name;
+  sampleFile.mv(uploadPath, function(err) {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    var numero = req.params.id
+    cloudinary.v2.uploader.upload(uploadPath,{public_id: numero},
+      function(err, result) {
+        client.set(numero, result.url, redis.print)
       });
-    }else{return res.status(404).send('No image to update.');}
+    res.send('La imagen con el codigo ' + numero + " ha sido actualizada");
   });
 });
 
+
 app.delete('/image/:id/', function(req, res) {
-  client.get(req.params.id,function(error, result){
-    if (fs.existsSync(result)) {
-      fs.unlink(result, (err) => {
-        if (err) throw err;
-        res.send('El archivo con codigo ' + req.params.id + ' ha sido eliminado');
-      });
-      client.del(req.params.id)
-		}else{return res.status(404).send('Image not found.');}
+  client.get(req.params.id,function(err,reply){
+    if (reply != null){
+      var numero = req.params.id
+      cloudinary.uploader.destroy(numero,
+        function(error, result){console.log(result, error)});
+      client.del(numero)
+      res.send('La imagen con el codigo ' + numero + " ha sido eliminada");
+    }else{return res.status(404).send('Image not found.')};
   });
 });
